e812c66c0c1d7ae29712c5ca7ee5660c558b67b4
Moctar <mctrba@gmail.com>
2018-07-03 20:41:47 -0500

    move launcher and clean up scripts in their own directories

---
+++ scripts/launcher.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Change this to your netid
+netid=mxb173430
+
+# Root directory of program and config file
+PROG=/home/013/m/mx/mxb173430/snapshot/node
+CONFIG=/home/013/m/mx/mxb173430/snapshot/conf3.txt
+
+# Directory where the config file is located on your local system
+CONFIGLOCAL=$HOME/launch/conf3.txt
+
+n=0
+
+# sed commands to ignore lines starting with # or _
+cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
+(
+    read -n 2 i
+    echo $i
+    read line
+    while [[ $n -lt $i ]]
+    do
+    	read line
+    	p=$( echo $line | awk '{ print $1 }' )
+	echo $p
+        host=$( echo $line | awk '{ print $2 }' )
+	echo $host
+	
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host.utdallas.edu $PROG $p $CONFIG &
+
+        n=$(( n + 1 ))
+    done
+)
